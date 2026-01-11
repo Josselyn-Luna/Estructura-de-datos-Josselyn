51b6d83b396e389dd64deb3232d98238ed1ebc32
lunaj <jm.lunac@uea.edu.ec>
2026-01-11 15:54:38 -0500

    Semana 5

---
+++ MisAsignaturas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Creamos una lista de strings para almacenar las asignaturas
+        List<string> asignaturas = new List<string> 
+        { 
+            "Matemáticas", 
+            "Física", 
+            "Química", 
+            "Historia", 
+            "Lengua" 
+        };
+
+        // Mostramos la lista por pantalla
+        Console.WriteLine("Asignaturas del curso:");
+        
+        // Usamos un bucle para recorrer la lista e imprimir cada elemento
+        foreach (string asignatura in asignaturas)
+        {
+            Console.WriteLine("- " + asignatura);
+        }
+    }
+}
